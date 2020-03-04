ed8a9ea025a618b229851942eb66893176c64432
HaiTV <haitv@ominext.com>
2020-03-04 23:22:13 +0700

    init client

---
+++ build/precache-manifest.eadc25daf3c18c4a71dd1a78000bdf39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6feede8573359c0947fc1e8881b1b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8ce2b3973a3d7e98818",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "22af0a4f551878c399ca",
+    "url": "/static/js/2.ff454c45.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ff454c45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8ce2b3973a3d7e98818",
+    "url": "/static/js/main.767707c3.chunk.js"
+  },
+  {
+    "revision": "7fe398bc21dc2fc92288",
+    "url": "/static/js/runtime-main.dba5d64a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
